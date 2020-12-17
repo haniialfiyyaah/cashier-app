8a1790b4af1817365b9c9a3e8c4d420f5322bd3b
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2020-12-17 16:03:20 +0700

    Add detail transaction on controller

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -8,6 +8,18 @@ class Controller {
             .then(data => res.send(data))
             .catch(err => res.send(err))
     }
+
+    static detail(req, res) {
+        Transaction
+        .findOne({
+            where : {
+              id: +req.params.id  
+            },
+            include: [Item],
+        })
+        .then(data => res.send(data))
+        .catch(err => res.send(err))
+    }
 }
 
 module.exports = Controller
--- routers/transactions.js
+++ routers/transactions.js
@@ -3,5 +3,6 @@ const Controller = require('../controllers/TransactionController')
 const router = require('express').Router()
 
 router.get('/', Controller.list)
+router.get('/:id/detail', Controller.detail)
 
 module.exports = router
