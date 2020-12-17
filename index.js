0bf72f4f0ec9fdf6f6b559376ac2a8675bc3cb67
syukurd <syukurbjj@yahoo.com>
2020-12-18 04:37:27 +1030

    siap beberapa view

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const session = require('express-session')
-const router = require('./routers')
-const app = express()
-const port = 3000
-app.use(session({
-    secret: 'hacktivpairproject-cashier',
+const express = require("express");
+const session = require("express-session");
+const router = require("./routers");
+const app = express();
+const port = 3000;
+app.use(
+  session({
+    secret: "hacktivpairproject-cashier",
     resave: false,
     saveUninitialized: true,
     // cookie: { secure: true }
-  }))
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(router)
+  })
+);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
 
-app.listen(port, () => {
-    console.log('Running on',port);
-})
+app.listen(process.env.PORT || port, () => {
+  console.log("Running on", port);
+});
